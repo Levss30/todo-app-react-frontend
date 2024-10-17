598f4b33c62051749d17849a2e8a9daf55491b5d
Lucas <lucas.levasseur18@gmail.com>
2024-10-17 14:14:38 -0300

    Refactor

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { LoginPage } from './Components/login-page/LoginPage';
+import TodoPage from './Components/todo-page/TodoPage';
+
+function App() {
+
+  return (
+    <LoginPage />
+  );
+}
+
+export default App;
