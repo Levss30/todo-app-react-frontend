8115cb4b2a7eb973165355ee9ee1036cb251da95
Lucas <lucas.levasseur18@gmail.com>
2024-10-18 15:20:47 -0300

    Feat Navigate

--- src/Components/login-page/LoginPage.jsx
+++ src/Components/login-page/LoginPage.jsx
@@ -2,16 +2,17 @@ import './LoginPage.css';
 import { FaUser } from "react-icons/fa6";
 import { FaLock } from "react-icons/fa";
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        
-        alert("Entrando com " + username + " " + password);
-    };
+        navigate('/home')
+};
 
 
   return (
@@ -23,12 +24,12 @@ export const LoginPage = () => {
                 <div className='login-input-item'>
                     <label>Email</label>
                     <FaUser id='icon'/>
-                    <input type='text' placeholder='User@exemple.com' onChange={(e) => setUsername(e.target.value)}/>
+                    <input type='text' placeholder='User@exemple.com' value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <div className='login-input-item'>
                     <label>Senha</label>
                     <FaLock id='icon'/>
-                    <input type="password" placeholder='Digite uma senha' onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" placeholder='Digite uma senha' value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <div className='esquecisenhaBtn'>
                     <label className='esquecisenha'>Recuperar senha</label>
                     </div>
