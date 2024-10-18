7781f369a11339d2b1d13b23db28aa353f6453dd
Lucas <lucas.levasseur18@gmail.com>
2024-10-17 21:54:26 -0300

    Feat

--- src/Components/login-page/LoginPage.jsx
+++ src/Components/login-page/LoginPage.jsx
@@ -1,4 +1,6 @@
 import './LoginPage.css';
+import { FaUser } from "react-icons/fa6";
+import { FaLock } from "react-icons/fa";
 
 export const LoginPage = () => {
 
@@ -10,10 +12,12 @@ export const LoginPage = () => {
                 <h1>Login</h1>
                 <div className='login-input-item'>
                     <label>Email</label>
+                    <FaUser className='icon1'/>
                     <input type='text' placeholder='User@exemple.com' />
                 </div>
                 <div className='login-input-item'>
                     <label>Senha</label>
+                    <FaLock id='icon2'/>
                     <input type="text" placeholder='Digite uma senha' />
                     <div className='esquecisenhaBtn'>
                     <label className='esquecisenha'>Recuperar senha</label>
