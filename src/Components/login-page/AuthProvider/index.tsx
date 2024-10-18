0b1c285aaf83cedac39ef84e9dbc042cc3fe1329
Lucas <lucas.levasseur18@gmail.com>
2024-10-18 12:42:32 -0300

    Feat Authenticate

--- src/Components/login-page/AuthProvider/index.tsx
+++ src/Components/login-page/AuthProvider/index.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
 import { IAuthProvider, IContext, IUser } from './types';
+import { LoginRequest } from './util';
 
 export const AuthContext = createContext<IContext>({} as IContext)
 
@@ -7,7 +8,11 @@ export const AuthProvider = ({ children }: IAuthProvider) =>{
     const [user, setUser] = useState<IUser>()
 
     async function authenticate(email: string, password: string){
+        const response = await LoginRequest(email, password)
 
+        const payload = {token: response.token, email}
+
+        setUser(payload);
     }
 
     function logout(){
