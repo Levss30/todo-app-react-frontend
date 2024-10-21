6f4c1fab77dc7b8fdf18a18615069cb395eff0d9
Lucas <lucas.levasseur18@gmail.com>
2024-10-21 15:58:34 -0300

    Feat Form

--- src/Components/cadastro-page/CadastroPage.jsx
+++ src/Components/cadastro-page/CadastroPage.jsx
@@ -6,7 +6,27 @@ const CadastroPage = () => {
         <div className='cadastropage'>
             <div className='cadastro-wrapper'>
                 <div className='cadastro-input'>
-                    <h1>Cadastro</h1>
+                    <h1>Create accont</h1>
+                </div>
+                <div className='cadastro-input-item'>
+                  <label>Nome</label>
+                  <input type="text" placeholder='Digite o seu nome' />
+                </div>
+                <div className='cadastro-input-item'>
+                  <label>Email</label>
+                  <input type="text" placeholder='Digite o seu email' />
+                </div>
+                <div className='cadastro-input-item'>
+                  <label>Senha</label>
+                  <input type="password" placeholder='Digite uma senha'/>
+                </div>
+                <div className='cadastro-input-item'>
+                  <label>Confirmar senha</label>
+                  <input type="password"  placeholder='confirme a senha'/>
+                </div>
+                <div className='cadastro-input-button'>
+                  <button className='confirmBtn'>Confirmar</button>
+                  <button className='voltarBtn'>Voltar</button>
                 </div>
             </div>
         </div>
