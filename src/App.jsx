add03cf89e4cc1d0728b4af2cc85c49011f85d1a
Lucas <lucas.levasseur18@gmail.com>
2024-10-21 15:58:19 -0300

    Feat Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { LoginPage } from './Components/login-page/LoginPage';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import TodoPage from './Components/todo-page/TodoPage';
+import CadastroPage from './Components/cadastro-page/CadastroPage';
 
 const Principal = () =>{
   return(
       <Routes>
         <Route path='/' element={<LoginPage />}/>
         <Route path='home' element={<TodoPage />}/>
+        <Route path='register' element={<CadastroPage />} />
       </Routes>
   
   
