40ff015444a312c5d7c55795461b63f2ab63aed4
Lucas <lucas.levasseur18@gmail.com>
2024-10-25 15:04:33 -0300

    Feat RouteForgot

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { LoginPage } from './Components/login-page/LoginPage';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import TodoPage from './Components/todo-page/TodoPage';
 import CadastroPage from './Components/cadastro-page/CadastroPage';
+import SenhaPage from './Components/senha-page/SenhaPage';
 
 const Principal = () =>{
   return(
@@ -9,6 +10,7 @@ const Principal = () =>{
         <Route path='/' element={<LoginPage />}/>
         <Route path='home' element={<TodoPage />}/>
         <Route path='register' element={<CadastroPage />} />
+        <Route path='forgot-password' element={<SenhaPage />} />
       </Routes>
   
   
