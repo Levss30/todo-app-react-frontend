a206501dfe4e6192d8b3f370f5ded29d6d7c594e
Lucas <lucas.levasseur18@gmail.com>
2024-10-28 23:22:06 -0300

    Refactor Wrapper

--- src/Components/senha-page/SenhaPage.jsx
+++ src/Components/senha-page/SenhaPage.jsx
@@ -9,15 +9,17 @@ const SenhaPage = () => {
             <div className='senha-wrapper'>
               <div className='wrapper-input'>
               <h1>Recuperar senha</h1>
+              </div>
+              <div className='wrapper'>
               <div className='wrapper-input-item'>
                 <label>Digite o seu email
-                  <input type="text" placeholder='email@teste.com'/>
+                  <input type="text" placeholder='meuemailteste@email.com'/>
                 </label>
+              </div>
               </div>
               <div className='wrapper-input-button'>
               <button className='returnBtn'>Voltar</button>
               <button className='nextBtn'>Avançar</button>
-              </div>
               </div>
             </div>
           </div>
