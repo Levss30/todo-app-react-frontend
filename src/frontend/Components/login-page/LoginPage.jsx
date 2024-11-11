0420305cfba2eb99ca71cb40b70ca6af3e62012a
Lucas <lucas.levasseur18@gmail.com>
2024-11-11 12:29:34 -0300

    Refactor LoginPage

---
+++ src/frontend/Components/login-page/LoginPage.jsx
@@ -0,0 +1,58 @@
+import './LoginPage.css';
+import { FaUser } from "react-icons/fa6";
+import { FaLock } from "react-icons/fa";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const LoginPage = () => {
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        navigate('/home')
+};
+
+    const handleRegister = (e) =>{
+        e.preventDefault();
+        navigate('/register')
+};
+
+    const handleForgotPassword = (e) =>{
+        e.preventDefault();
+        navigate('/forgot-password')
+    }
+
+
+  return (
+<div className='background'>
+    <div className='loginpage'>
+        <div className='login-wrapper' onSubmit={handleSubmit}>
+            <div className='login-input'>
+                <h1>Login</h1>
+                <div className='login-input-item'>
+                    <label>Email</label>
+                    <FaUser id='icon'/>
+                    <input type='email' placeholder='User@exemple.com' value={username} onChange={(e) => setUsername(e.target.value)} required/>
+                </div>
+                <div className='login-input-item'>
+                    <label>Senha</label>
+                    <FaLock id='icon'/>
+                    <input type="password" placeholder='Digite uma senha' value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                    <div className='esquecisenhaBtn'>
+                    <label className='esquecisenha' onClick={handleForgotPassword}>Recuperar senha</label>
+                    </div>
+                </div>
+                <div className='login-input-item'>
+                    <button className='loginBtn' onClick={handleSubmit}>Entrar</button>
+                    <label className='lblregister'>Não tem uma conta ?
+                    <button className='registerBtn' onClick={handleRegister}>Cadastrar-se</button>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
