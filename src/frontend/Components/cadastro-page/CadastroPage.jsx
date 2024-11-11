e1125af821225a4520e249dd7e5f8c884ab78473
Lucas <lucas.levasseur18@gmail.com>
2024-11-11 12:41:10 -0300

    Refactor CadastroPage

--- src/frontend/Components/cadastro-page/CadastroPage.jsx
+++ src/frontend/Components/cadastro-page/CadastroPage.jsx
@@ -6,8 +6,7 @@ const CadastroPage = () => {
   const [formdata, setFormData] = useState({
     name: '',
     email: '',
-    senha: '',
-    confirmsenha: ''
+    senha: ''
   })
   const navigate = useNavigate();
 
@@ -21,7 +20,7 @@ const CadastroPage = () => {
   const handleSubmit = async (e) =>{
     try {
       e.preventDefault()
-      const response = await fetch(`localhost:5052/Usuarios/Registrar`, {
+      const response = await fetch(`localhost:5052/Usuarios/Registrar/`, {
         method: 'POST',
         body: JSON.stringify(formdata)
       })
@@ -46,19 +45,15 @@ const CadastroPage = () => {
                     <h1>Cadastro</h1>
                 <div className='cadastro-input-item'>
                   <label>Nome</label>
-                  <input type="text" placeholder='Digite o seu nome' required value={formdata.name} onChange={(e) => {handleFormEdit(e, 'name')}}/>
+                  <input type="text" placeholder='Digite o seu nome' required value={formdata.name} onChange={(e) => {handleFormEdit(e, 'Name')}}/>
                 </div>
                 <div className='cadastro-input-item'>
                   <label>Email</label>
-                  <input type="email" placeholder='Digite o seu email' required value={formdata.email} onChange={(e) => {handleFormEdit(e, 'email')}}/>
+                  <input type="email" placeholder='Digite o seu email' required value={formdata.email} onChange={(e) => {handleFormEdit(e, 'Email')}}/>
                 </div>
                 <div className='cadastro-input-item'>
                   <label>Senha</label>
-                  <input type="password" placeholder='Digite uma senha' required value={formdata.senha} onChange={(e) => {handleFormEdit(e, 'senha')}}/>
-                </div>
-                <div className='cadastro-input-item'>
-                  <label>Confirmar senha</label>
-                  <input type="password"  placeholder='confirme a senha' required value={formdata.confirmsenha} onChange={(e) => {handleFormEdit(e, 'confirmsenha')}}/>
+                  <input type="password" placeholder='Digite uma senha' required value={formdata.senha} onChange={(e) => {handleFormEdit(e, 'Senha')}}/>
                 </div>
                 <label className='checkLbl'>
                     <input type="checkbox" name="Terms" className='terms' required/>
